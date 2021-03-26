3f1857bb6fce474bf9a31e889462894ff1ae4d70
osman <osmankocak212@gmail.com>
2021-03-27 00:01:19 +0200

    update  (logın)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ht/screens/login.dart';
+import 'package:ht/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: login_page(),
+      home: LoginPage(),
     );
   }
 }
--- lib/services/api_manager.dart
+++
@@ -1,7 +0,0 @@
-import 'package:http/http.dart' as http;
-
-class api_manager {
-  void getNews() {
-    var client = http.Client();
-  }
-}
