b0bdf2cded6ff4650709f5916d0bd82d01dd46b2
buguhooxx <53261898+buguhooxx@users.noreply.github.com>
2021-03-27 00:04:16 +0300

    11

--- Our-E-School-master/Functions/TypeScript/functions/src/UserType.ts
+++
@@ -1,6 +0,0 @@
-export enum UserType {
-    STUDENT = "STUDENT",
-    TEACHER = "TEACHER",
-    PARENT = "PARENT",
-    UNKNOWN = "UNKNOWN"
-}
--- Our-E-School-master/android/app/src/main/kotlin/com/ketanchoyal/our_e_school/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ketanchoyal.our_e_school
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
